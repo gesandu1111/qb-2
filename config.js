1a6d41bdf6097ef724ebf31225ce07c850bd139b
gesandu1111 <gesandusanmira@gmail.com>
2026-01-02 21:25:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "8IlEUSKK#oUIn36mRbSxbX7kNBUIuw4PkYixNoLK1lMDPcz0omW8",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:xUAyTAIxyzpPZhrowiekKaBHkjSCMjEw@turntable.proxy.rlwy.net:57791",
+  OWNER_NUM: process.env.OWNER_NUM || "94753362282",
 };
